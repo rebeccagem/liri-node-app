e3d016ace1b33567e182fca66d88742844dc831d
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-04-04 23:32:54 -0500

    working OMDB search

--- liri.js
+++ liri.js
@@ -12,6 +12,7 @@ var client = new Twitter(keys.twitter);
 
 var arg1 = process.argv[2];
 var arg2 = process.argv[3];
+var arg3 = process.argv[3];
 
 
 // my-tweets
@@ -27,18 +28,33 @@ switch (arg1) {
         });
         break;
     case 'this-song':
-        spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
+        spotify.search({ type: 'track', query: arg3 }, function (err, data) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
-            console.log("Artist(s): "+data.tracks.items[0].artists[0].name);
-            console.log("Song Title: "+data.tracks.items[0].name);
-            console.log("Listen Here: "+data.tracks.items[0].album.external_urls.spotify);
-            console.log("Name of Album: "+data.tracks.items[0].album.name);
+            console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
+            console.log("Song Title: " + data.tracks.items[0].name);
+            console.log("Listen Here: " + data.tracks.items[0].album.external_urls.spotify);
+            console.log("Name of Album: " + data.tracks.items[0].album.name);
         });
         break;
     case 'movie-this':
-        console.log("omdb");
+        // Then run a request to the OMDB API with the movie specified
+        request("https://www.omdbapi.com/?t=" + arg3 + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+            // If the request is successful (i.e. if the response status code is 200)
+            if (!error && response.statusCode === 200) {
+                // Parse the body of the site and recover just the imdbRating
+                // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+                console.log("Title: " + JSON.parse(body).Title);
+                console.log("Release Year: " + JSON.parse(body).Year);
+                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                console.log("Country: " + JSON.parse(body).Country);
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+                // console.log(JSON.parse(body));
+            }
+        });
         break;
     case 'random':
         console.log("random");
