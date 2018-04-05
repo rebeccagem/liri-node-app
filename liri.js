c7319511a28f1e41065cbae7fd4924db886699d3
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-04-04 23:59:18 -0500

    working fs random call

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var request = require("request");
 var keys = require("./keys.js");
+var fs = require("fs");
 
 
 var spotify = new Spotify(keys.spotify);
@@ -29,40 +30,24 @@ switch (arg1) {
         });
         break;
     case 'this-song':
-        spotify.search({ type: 'track', query: arg3 }, function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
-            console.log("------------------------------------");
-            console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
-            console.log("Song Title: " + data.tracks.items[0].name);
-            console.log("Listen Here: " + data.tracks.items[0].album.external_urls.spotify);
-            console.log("Name of Album: " + data.tracks.items[0].album.name);
-            console.log("------------------------------------");
-        });
+        song();
         break;
     case 'movie-this':
-        // Then run a request to the OMDB API with the movie specified
-        request("https://www.omdbapi.com/?t=" + arg3 + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-            // If the request is successful (i.e. if the response status code is 200)
-            if (!error && response.statusCode === 200) {
-                // Parse the body of the site and recover just the imdbRating
-                // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-                console.log("------------------------------------");
-                console.log("Title: " + JSON.parse(body).Title);
-                console.log("Release Year: " + JSON.parse(body).Year);
-                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-                console.log("Country: " + JSON.parse(body).Country);
-                console.log("Plot: " + JSON.parse(body).Plot);
-                console.log("Actors: " + JSON.parse(body).Actors);
-                console.log("------------------------------------");
-                // console.log(JSON.parse(body));
-            }
-        });
+        movie();
         break;
     case 'random':
-        console.log("random");
+        // The code will store the contents of the reading inside the variable "data"
+        fs.readFile("random.txt", "utf8", function (error, data) {
+
+            // If the code experiences any errors it will log the error to the console.
+            if (error) {
+                return console.log(error);
+            }
+            // Then split it by commas (to make it more readable)
+            var dataArr = data.split(",");
+            arg3 = dataArr[1];
+            song();
+        });
 
 }
 // spotify-this-song
@@ -76,3 +61,38 @@ switch (arg1) {
 // movie-this
 
 // do-what-it-says
+
+function movie() {
+    // Then run a request to the OMDB API with the movie specified
+    request("https://www.omdbapi.com/?t=" + arg3 + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            console.log("------------------------------------");
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("------------------------------------");
+            // console.log(JSON.parse(body));
+        }
+    });
+}
+
+function song() {
+    spotify.search({ type: 'track', query: arg3 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log("------------------------------------");
+        console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
+        console.log("Song Title: " + data.tracks.items[0].name);
+        console.log("Listen Here: " + data.tracks.items[0].album.external_urls.spotify);
+        console.log("Name of Album: " + data.tracks.items[0].album.name);
+        console.log("------------------------------------");
+    });
+}
