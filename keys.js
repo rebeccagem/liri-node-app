13a4800933e342ebe039a483d685dd4bf593e811
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-04-04 21:58:23 -0500

    working twitter search

--- keys.js
+++ keys.js
@@ -1,16 +1,14 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.aixou3tDUilLfCjILDBW2dBEn,
-  consumer_secret: process.env.s9PGJTA4CIdXd1kWFdqu0bClvsnzhrUROZ8wjAIA8B0xyIsIOq,
-  access_token_key: process.env.rLpxX07UTHcOPfmz7NIb5iAFbr2sYOuLWJOanyv,
-  access_token_secret: process.env.MhatXSczVQbfynM2NY19X64ATWxGc9PRC5J2SknH1VMq9
-
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
-
-// 1260078398-rLpxX07UTHcOPfmz7NIb5iAFbr2sYOuLWJOanyv
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
--- liri.js
+++ liri.js
@@ -1,11 +1,41 @@
 require("dotenv").config();
 
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var request = require("request");
+var keys = require("./keys.js");
+
+
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+
+var arg1 = process.argv[2];
+var arg2 = process.argv[3];
+
+
 // my-tweets
 // This will show your last 20 tweets and when they were created at in your terminal/bash window.
+switch (arg1) {
+    case 'my-tweets':
+        client.get('statuses/user_timeline', function (error, tweets, response) {
+            if (error) throw error;
+                 for (var i = 0; i < 3; i++) {
+                    console.log("Tweet: " + tweets[i].text);
+                    console.log("Time Stamp: " + tweets[i].user.created_at);
+            }
+        });
+        break;
+    case 'this-song':
+        console.log("spotify");
+        break;
+    case 'movie-this':
+        console.log("omdb");
+        break;
+    case 'random':
+        console.log("random");
 
+}
 // spotify-this-song
 //This will show the following information about the song in your terminal/bash window
 // - Artist(s)
