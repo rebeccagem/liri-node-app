4d14669e5d98c098fe360305b23103925c549895
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-04-05 18:15:54 -0500

    some comments on code and default search results with no 2nd argument

--- liri.js
+++ liri.js
@@ -10,11 +10,9 @@ var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-
+//grabs the arugments passed in the command line
 var arg1 = process.argv[2];
 var arg2 = process.argv[3];
-var arg3 = process.argv[3];
-
 
 // my-tweets
 // This will show your last 20 tweets and when they were created at in your terminal/bash window.
@@ -29,12 +27,18 @@ switch (arg1) {
             }
         });
         break;
+    // spotify-this-song
+    //This will show the following information about the song in your terminal/bash window
+    // - Artist(s), The song's name, A preview link of the song from Spotify, The album that the song is from
+    // - If no song is provided then your program will default to "The Sign" by Ace of Base.
     case 'this-song':
         song();
         break;
+    //movie-this shows movie info from OMDB        
     case 'movie-this':
         movie();
         break;
+        // do-what-it-says pulls info from random.txt to run as spotify search command
     case 'random':
         // The code will store the contents of the reading inside the variable "data"
         fs.readFile("random.txt", "utf8", function (error, data) {
@@ -45,26 +49,20 @@ switch (arg1) {
             }
             // Then split it by commas (to make it more readable)
             var dataArr = data.split(",");
-            arg3 = dataArr[1];
+            arg2 = dataArr[1];
             song();
         });
 
 }
-// spotify-this-song
-//This will show the following information about the song in your terminal/bash window
-// - Artist(s)
-// - The song's name
-// - A preview link of the song from Spotify
-// - The album that the song is from
-// - If no song is provided then your program will default to "The Sign" by Ace of Base.
 
-// movie-this
-
-// do-what-it-says
 
 function movie() {
+    //checks to see if there is no 2nd arg, if not, then it shows the default search 
+    if (!arg2) {
+        arg2 = "Mr. Nobody";
+    }
     // Then run a request to the OMDB API with the movie specified
-    request("https://www.omdbapi.com/?t=" + arg3 + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+    request("https://www.omdbapi.com/?t=" + arg2 + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
         // If the request is successful (i.e. if the response status code is 200)
         if (!error && response.statusCode === 200) {
             // Parse the body of the site and recover just the imdbRating
@@ -84,7 +82,11 @@ function movie() {
 }
 
 function song() {
-    spotify.search({ type: 'track', query: arg3 }, function (err, data) {
+    //checks to see if there is no 2nd arg, if not, then it shows the default search 
+    if (!arg2) {
+        arg2 = "Ace of Base";
+    }
+    spotify.search({ type: 'track', query: arg2 }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
